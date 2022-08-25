2b5fb70e44312b6e9e2942d5e1759bda740f50e2
jhlz <jhlz99520@163.com>
2022-08-25 22:27:45 +0800

    feat: add resources reader

--- src/main/java/jhlz/code/resources/ReaderResources.java
+++ src/main/java/jhlz/code/resources/ReaderResources.java
@@ -1,7 +1,26 @@
-package jhlz.code.resources;/**
+package jhlz.code.resources;
+
+import java.io.File;
+
+/**
  * @author jhlz
  * @time: 2022/8/25 下午9:45
  * @desc: ReaderResources
  */
 public class ReaderResources {
+    // private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(ReaderResources.class);
+
+    public static void main(String[] args) {
+
+        File is = new File("src/main/resources/img");
+        System.out.println(is.toPath());
+        System.out.println(is.getPath());
+        File[] files = is.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            // 获取文件名称 包括后缀
+            System.out.println(files[i].getName());
+            // 获取文件绝对路径
+            System.out.println(files[i].getAbsoluteFile());
+        }
+    }
 }
