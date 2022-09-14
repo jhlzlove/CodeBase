d60ea2b302dc59687d7c8bc29216972666aba288
jhlz <jhlzlove@163.com>
2022-09-14 22:43:27 +0800

    test: :white_check_mark: add test example

---
+++ src/test/java/JDKTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * JDKTest:
+ *
+ * @author jhlz
+ * @since 2022/9/14 19:25:21
+ */
+public class JDKTest {
+    /**
+     * test：MathRound用例
+     */
+    @Test
+    public void testMathRound() {
+        new ReentrantLock(true);
+        System.out.println(2 << 3);
+        System.out.println(Math.round(11.5));
+        System.out.println(Math.round(11.4));
+        System.out.println(Math.round(-11.5));
+    }
+}
