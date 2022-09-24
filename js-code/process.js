c335b78516dcca281790b1e32521a683ae3992df
jhlz <jhlzlove@163.com>
2022-09-24 22:01:21 +0800

    refactor: :recycle: update js-code process

--- js-code/process.js
+++ js-code/process.js
@@ -17,4 +17,24 @@ console.log(process.env.USERPROFILE);
 console.log(process.argv);
 console.log(process.argv0);
 console.log(process.pid);*/
+// process.argv 返回一个数组，第一个参数为执行命令的目录
+// 第二个参数为执行文件的目录
+// 如果后面还有参数的话，也会一并输出
+/**
+ * 例如：node process.js arg1 arg2 hahah
+ * 返回结果如下：
+ * [
+ *   'D:\\xxxx\\nodejs\\node.exe',
+ *   'D:\\xxx\\js-code\\process.js',
+ *   'arg1',
+ *   'arg2',
+ *   'hahah'
+ * ]
+ */
+console.log(process.argv)
+console.log(process.pid)
 
+// 打印后面的参数
+if (process.argv[2]) {
+    console.log(process.argv[2])
+}
